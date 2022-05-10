f04ad416a32e16ef546677970485e3983063b979
initial@gmail.com <teamdailypractice@gmail.com>
2022-05-10 17:28:52 +0800

    jest testing

--- ex02/Cart.test.js
+++ ex02/Cart.test.js
@@ -1,9 +1,18 @@
-const assert = require('assert');
 const Cart = require('./Cart');
 
 
 test("add an item to the empty cart", () => {
     const cart = new Cart();
     cart.addToCart("cheesecake");
-    assert.deepStrictEqual(cart.items, ['cheesecake']);
+    expect(cart.items).toEqual(['cheesecake']);
+
+});
+
+
+test("remove the item from the cart which has 1 item", () => {
+    const cart = new Cart();
+    cart.addToCart("cheesecake");
+    cart.removeFromCart("cheesecake");
+
+    expect(cart.items).toEqual([]);
 });
