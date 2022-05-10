8304a508fb2bfe0becee2d455901556e85dccc71
initial@gmail.com <teamdailypractice@gmail.com>
2022-05-10 15:54:48 +0800

    Cart add and remove functions

---
+++ ex01/Cart.js
@@ -0,0 +1,20 @@
+class Cart {
+    constructor() {
+        this.items = [];
+    }
+
+    addToCart(item) {
+        this.items.push(item);
+    }
+
+    /* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+    removeFromCart(item) {
+        for (let i = 0; i < this.items.length; i++) {
+            const currentItem = this.items[i];
+            if (currentItem === item) {
+                this.items.splice(i, 1);
+            }
+
+        }
+    }
+}
